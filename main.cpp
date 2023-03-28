4a91c26471484657037f23d613042139187abde7
Hacene YOUNSI <hacene.younsi@yahoo.com>
2023-03-28 22:58:48 +0200

    Add opencv client receiver

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+#include <opencv2/opencv.hpp>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <iostream>
 #include <string>
 
 #include "Socket.hpp"
 
+#define PORT 5000 // Port number to connect to
+#define WIDTH 640 // Width of the video stream
+#define HEIGHT 480 // Height of the video stream
+
 using namespace service::system::network;
+using namespace cv;
 
 int main()
 {
@@ -11,17 +21,61 @@ int main()
     clientSocket.connect("192.168.1.67", 5000);
     std::cout << "Connected to server!\n";
 
-    //std::string message;
+    // Loop to receive frames from the server and display them
     while (true)
     {
-        char sendBuf[1024];
-        std::cout << "Message to send : \n";
-        std::cin.getline(sendBuf, sizeof(sendBuf));
+        Mat frame(HEIGHT, WIDTH, CV_8UC3);
+        int size = frame.total() * frame.elemSize();
+        int received = 0;
+        while (received < size)
+        {
+            int bytes = clientSocket.recv(sockfd, frame.data + received, size - received, 0);
+            if (bytes == -1)
+            {
+                cerr << "Unable to receive the frame data" << endl;
+                break;
+            }
+            received += bytes;
+        }
 
-        clientSocket.send(sendBuf, sizeof(sendBuf));
-        char buf[1024] = {0};
-        int n = clientSocket.recv(buf, sizeof(buf));
-        std::cout << "Received message: " << buf << "\n";
+        // Check if the connection has been closed
+        if (received == 0)
+        {
+            cerr << "Connection closed by the server" << endl;
+            break;
+        }
+
+        // Convert to grayscale
+        cv::Mat gray;
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        // Apply thresholding
+        cv::Mat thresh;
+        cv::threshold(gray, thresh, 128, 255, cv::THRESH_BINARY);
+
+        // Find contours
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(thresh, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        // Iterate over contours
+        for (auto& contour : contours) {
+            // Approximate contour as a polygon
+            std::vector<cv::Point> polygon;
+            cv::approxPolyDP(contour, polygon, 10, true);
+
+            // If polygon has four sides, it's likely to be a rectangle
+            if (polygon.size() == 4) {
+                // Draw rectangle on frame
+                cv::polylines(frame, polygon, true, cv::Scalar(0, 255, 0), 2);
+            }
+        }
+
+        // Display the received frame
+        imshow("Video Stream", frame);
+
+        // Wait for a key press to exit the program
+        if (waitKey(1) == 27)
+            break;
     }
 
 /*
